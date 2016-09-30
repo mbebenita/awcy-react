83eb916aeb0a9a6686f855527500e9a197b241fa
Michael Bebenita <mbebenita@gmail.com>
2016-09-30 14:25:53 -0700

    Merge tabs on small devices.

--- src/App.tsx
+++ src/App.tsx
@@ -44,54 +44,71 @@ export class App extends React.Component<void, void> {
     console.debug("Rendering App");
 
     let height = window.innerHeight;
+    let width = window.innerWidth;
+
+    let sidebarTabs = [
+      <Tab eventKey={1} key="runs" title="Runs">
+        <div style={{ padding: 10 }}>
+          <JobsComponent showFilters jobStatusFilter={JobStatus.Completed} jobs={appStore.jobs} listHeight={height - 200} />
+        </div>
+      </Tab>,
+      <Tab eventKey={2} key="jobs" title="Jobs">
+        <div style={{ padding: 10 }}>
+          <JobsComponent showCommands jobStatusFilter={JobStatus.NotCompleted} jobs={appStore.jobs} listHeight={height - 200} />
+        </div>
+      </Tab>,
+      <Tab eventKey={3} key="share" title="Share">
+        <div style={{ padding: 10 }}>
+          <ShareComponent/>
+        </div>
+      </Tab>,
+      <Tab eventKey={4} key="login" title="Login">
+        <div style={{ padding: 10 }}>
+          <LoginComponent/>
+        </div>
+      </Tab>
+    ];
+
+    let contentTabs = [
+      <Tab eventKey={3} key="graphs" title="Report">
+        <div style={{ padding: 10, height: height - 100, overflow: "scroll" }}>
+          <FullReportComponent jobs={appStore.selectedJobs} />
+        </div>
+      </Tab>,
+      <Tab eventKey={4} key="logs" title="Logs">
+        <div style={{ padding: 10, height: height - 100, overflow: "scroll" }}>
+          <AppLogsComponent/>
+        </div>
+      </Tab>,
+      <Tab eventKey={5} key="status" title="Status">
+        <div style={{ padding: 10, height: height - 100, overflow: "scroll" }}>
+          <AppStatusComponent/>
+        </div>
+      </Tab>,
+      <Tab eventKey={6} key="debug" title="Debug">
+        <div style={{ padding: 10, height: height - 100, overflow: "scroll" }}>
+          <DebugComponent/>
+        </div>
+      </Tab>
+    ];
+
+    if (width < 1024) {
+      return <div>
+        <Tabs defaultActiveKey={1} animation={false} id="noanim-tab-example">
+          {sidebarTabs}
+          {contentTabs}
+        </Tabs>
+      </div>
+    } else
     return <div>
       <div className="sidebar">
         <Tabs defaultActiveKey={1} animation={false} id="noanim-tab-example">
-          <Tab eventKey={1} key="runs" title="Runs">
-            <div style={{ padding: 10 }}>
-              <JobsComponent showFilters jobStatusFilter={JobStatus.Completed} jobs={appStore.jobs} listHeight={height - 200} />
-            </div>
-          </Tab>
-          <Tab eventKey={2} key="jobs" title="Jobs">
-            <div style={{ padding: 10 }}>
-              <JobsComponent showCommands jobStatusFilter={JobStatus.NotCompleted} jobs={appStore.jobs} listHeight={height - 200} />
-            </div>
-          </Tab>
-          <Tab eventKey={3} key="share" title="Share">
-            <div style={{ padding: 10 }}>
-              <ShareComponent/>
-            </div>
-          </Tab>
-          <Tab eventKey={4} key="login" title="Login">
-            <div style={{ padding: 10 }}>
-              <LoginComponent/>
-            </div>
-          </Tab>
+          {sidebarTabs}
         </Tabs>
       </div>
       <div className="content">
         <Tabs defaultActiveKey={3} animation={false} id="noanim-tab-example">
-          <Tab eventKey={3} key="graphs" title="Report">
-            <div style={{ padding: 10, height: height - 100, overflow: "scroll" }}>
-              <FullReportComponent jobs={appStore.selectedJobs} />
-            </div>
-          </Tab>
-          <Tab eventKey={4} key="logs" title="Logs">
-            <div style={{ padding: 10, height: height - 100, overflow: "scroll" }}>
-              <AppLogsComponent/>
-            </div>
-          </Tab>
-          <Tab eventKey={5} key="status" title="Status">
-            <div style={{ padding: 10, height: height - 100, overflow: "scroll" }}>
-              <AppStatusComponent/>
-            </div>
-          </Tab>
-          <Tab eventKey={6} key="debug" title="Debug">
-            <div style={{ padding: 10, height: height - 100, overflow: "scroll" }}>
-              <DebugComponent/>
-            </div>
-          </Tab>
-          {analyzerTabs}
+          {contentTabs}
         </Tabs>
       </div>
     </div>
